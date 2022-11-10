07573853a459958c3b27dcc996cdf6693c231032
Yasir <yasirahmed1995@gmail.com>
2022-11-10 13:45:59 +0400

    search Button Added

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -8,15 +8,25 @@ const SearchBar = ({ onFormSubmit }) => {
     onFormSubmit(term);
   };
   return (
-    <form onSubmit={onSubmit} className="my-3">
-      <input
-        className="form-control form-control-lg"
-        type="text"
-        placeholder="Video Search"
-        aria-label=".form-control-lg example"
-        value={term}
-        onChange={(e) => setTerm(e.target.value)}
-      ></input>
+    <form
+      onSubmit={onSubmit}
+      className="row my-3 d-flex align-items-center justify-content-between"
+    >
+      <div className="col-9 col-md-10">
+        <input
+          className="form-control form-control-lg"
+          type="text"
+          placeholder="Video Search"
+          aria-label=".form-control-lg example"
+          value={term}
+          onChange={(e) => setTerm(e.target.value)}
+        ></input>
+      </div>
+      <div className="col-3 col-md-2">
+        <button type="submit" class="btn btn-primary">
+          Search
+        </button>
+      </div>
     </form>
   );
 };
